b6ba043d467e6f8034dbf094b2cac31b88f35165
yaokm <yaokm320@gmail.com>
2020-09-14 20:11:55 +0800

    update

--- maven-service/src/main/java/com/example/ssm/service/impl/ItemServiceImpl.java
+++ maven-service/src/main/java/com/example/ssm/service/impl/ItemServiceImpl.java
@@ -9,21 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class ItemServiceImpl implements ItemService {
+public class ItemServiceImpl implements ItemService{
 
     @Autowired
     private ItemMapper itemMapper;
 
-    public ItemMapper getItemMapper() {
-        return itemMapper;
-    }
-
-    public void setItemMapper(ItemMapper itemMapper) {
-        this.itemMapper = itemMapper;
-    }
-
-    @Override
     public Item findById(int id) {
         return itemMapper.findById(id);
     }
 }
+
--- maven-web/src/main/java/com/example/ssm/controller/ItemController.java
+++ maven-web/src/main/java/com/example/ssm/controller/ItemController.java
@@ -3,8 +3,8 @@ package com.example.ssm.controller;
 
 import com.example.ssm.pojo.Item;
 import com.example.ssm.service.ItemService;
+import com.example.ssm.service.impl.ItemServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ItemController {
 
     @Autowired
+    /* "类型"的解释：
+    * Spring注解是通过动态代理实现的，java中有两种动态代理的方式：基于JDK的动态代理和基于Cglib的动态代理
+    * 由于ItemServiceImpl实现了接口，因此这里java虚拟机自动使用了基于JDK的动态代理
+    * 所以这里的类型必须是接口，而不能是类。如果ItemService直接写成一个类，这里的类型就可以是类。
+    * */
     private ItemService itemService;
 
     @RequestMapping("/showItem/{id}")
